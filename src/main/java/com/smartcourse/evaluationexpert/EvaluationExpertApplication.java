7f6d5ba3f2bfc5f5b837168ccb5e2ff5496e6406
32358 <3235805272@qq.com>
2025-11-12 20:46:39 +0800

    初始化后端

---
+++ src/main/java/com/smartcourse/evaluationexpert/EvaluationExpertApplication.java
@@ -0,0 +1,13 @@
+package com.smartcourse.evaluationexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationExpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationExpertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartcourse/evaluationexpert/EvaluationExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcourse.evaluationexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
