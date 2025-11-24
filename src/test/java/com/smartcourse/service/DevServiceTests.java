95b34d94356b1e35666dbe5ccc05eb3dbd303709
32358 <3235805272@qq.com>
2025-11-24 19:21:09 +0800

    微量更改

--- src/test/java/com/smartcourse/service/DevServiceTests.java
+++ src/test/java/com/smartcourse/service/DevServiceTests.java
@@ -1,16 +1,26 @@
 package com.smartcourse.service;
 
+import com.smartcourse.model.QuestionKnowledgeDocument;
+import com.smartcourse.pojo.entity.KnowledgeNode;
+import com.smartcourse.repository.QuestionKnowledgeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class DevServiceTests {
     @Autowired
     private DevService devService;
+    @Autowired
+    private QuestionKnowledgeRepository questionKnowledgeRepository;
+
     @Test
     void testDevService(){
         devService.updateQuestionKnowledge(1L,231L,481L);
+        Optional<QuestionKnowledgeDocument> id = questionKnowledgeRepository.findById(1L);
     }
+
 
 }
